0656d62f42125232a4c2023e1d760ce2eade1d60
Gyunald <bigstar8897@naver.com>
2022-01-04 23:46:57 +0900

    2022_01_04

---
+++ quiz_3.py
@@ -0,0 +1,19 @@
+from random import *
+import random
+
+c = 1
+f = []
+
+# t = list(range(5,15+1))
+while c < 50 + 1 :
+    t = randrange(5,50+1)
+    if  5 <= t <= 15 :
+        print(f"[O] {c}번째 손님 (소요시간 : {t}분)")
+        f.append(c)
+        c += 1
+
+    elif t > 15 :
+        print(f"[X] {c}번째 손님 (소요시간 : {t}분)")
+        c += 1
+
+print(f"총 탑승 승객 {len(f)}분")
