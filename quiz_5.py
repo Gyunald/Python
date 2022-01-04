26bd92dc84d011095ca84d969b85b3f45c19a525
kyudeok <bigstar8897@naver.com>
2022-01-04 22:16:58 +0900

    2022_01_04

--- quiz_5.py
+++ quiz_5.py
@@ -1,4 +1,4 @@
-for i in range(1,51) :
+for i in range(1,5) :
     with open(f"{i}주차.txt", "w", encoding="utf-8") as a :
-        print(a.write(f"- {i}주차 주간보고 -\n부서 :\n이름 :\n업무 요약 :"),end="")
+        a.write(f"- {i}주차 주간보고 -\n부서 :\n이름 :\n업무 요약 :")
 print(a)
