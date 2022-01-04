9a3b566efa3141e194d73d230f0c1bd0a12f0105
kyudeok <bigstar8897@naver.com>
2022-01-04 19:13:43 +0900

    a

--- quiz_4.py
+++ quiz_4.py
@@ -12,7 +12,7 @@ gender = "남자"
 weight = round(std_weight(height,gender),2)
 print(f"키 {height}cm 남자의 표준 체중은 {weight} 입니다")
 
-# 표준 체중
+# # 표준 체중
 
 # def std_weight(height, gender) :
 #     if gender == "남자" :
@@ -21,4 +21,5 @@ print(f"키 {height}cm 남자의 표준 체중은 {weight} 입니다")
 #         return (height / 100) * (height / 100) * 21
 
 # weight = round(std_weight(175,"남자"),2)
-# print(f"키 {???}cm 남자의 표준 체중은 {weight} 입니다")
+# height = ?
+# print(f"키 {height}cm 남자의 표준 체중은 {weight} 입니다")
