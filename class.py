f95b2476c13ced32333de5576149c549859aa144
Gyunald <bigstar8897@naver.com>
2022-01-13 22:58:41 +0900

    2022-01-13

--- class.py
+++ class.py
@@ -1,15 +1,15 @@
 class Unit:
-    def __init__(self, name, hp, damage):
+    def __init__(self, name, hp, speed):
         self.name = name
         self.hp = hp
-        self.damage = damage
-        print(f"{self.name} 유닛이 생성되었습니다.")
-        print(f"체력 {self.hp}, 공격력 {self.damage}")
-
-class AttackUnit:
-    def __init__(self, name, hp, damage):
-        self.name = name
-        self.hp = hp
+        self.speed = speed
+    
+    def move(self, location):
+        print(f"[지상 유닛 이동]")
+        print(f"{self.name} : {location} 방향으로 이동합니다. [속도 {self.speed}")
+class AttackUnit(Unit):
+    def __init__(self, name, hp, speed, damage):
+        Unit.__init__(self, name, hp, speed)
         self.damage = damage
 
     def attack(self, location):
@@ -21,8 +21,24 @@ class AttackUnit:
         print(f"{self.name} : 현재 체력은 {self.hp} 입니다.")
         if self.hp <= 0 :
             print(f"{self.name} : 파괴되었습니다")
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+    def fly(self, name, location):
+        print(f"{name} : {location} 방향으로 날아갑니다. [속도 {self.flying_speed}]")
+
+class FlybleAttackUnit(AttackUnit,Flyable):
+    def __init__(self, name, hp, damage, flying_speed) :
+        AttackUnit.__init__(self, name, hp, 0, damage)
+        Flyable.__init__(self, flying_speed)
+    
+    def move(self, location):
+        print(f"[공중 유닛 이동]")
+        self.fly(self.name, location)
 
 
-firebat1 = AttackUnit("파이어뱃", 50, 16)
-firebat1.attack("5시")
-firebat1.damaged(50)       
+vulture = AttackUnit("벌쳐", 80, 10, 20)
+battlecruiser = FlybleAttackUnit("배틀크루저", 500 , 25, 3)
+
+vulture.move("11시")
+battlecruiser.move("9시")
