f34a98cc682bd739d84e92082126708065b903bd
Gyunald <bigstar8897@naver.com>
2022-01-17 22:25:25 +0900

    2022-01-17

---
+++ 1_module.py
@@ -0,0 +1,15 @@
+# import theater_module
+# theater_module.price(3)
+
+# import theater_module as mv
+# mv.price_morning(2)
+
+# from theater_module import *
+# price_soldier(1)
+
+# from theater_module import price, price_morning
+# price(5)
+# price_morning(6)
+
+from theater_module import price_soldier as soldier
+soldier(3)
