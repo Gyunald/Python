634906cdc16b7b492a949ef45b000cc57ebaa56f
Gyunald <bigstar8897@naver.com>
2022-02-27 20:26:01 +0900

    2022-02-27

--- practice_10.py
+++ practice_10.py
@@ -125,19 +125,118 @@
 # value = return_test()
 # print(value)
 
-# def f(x) :
+# def f(x):
 #     return (2 * x) + 1
-# print(f(3))
-
-# def f(x) :
-#     return (x * x) + (2 * x) + 1
-# print(f(3))
+# print(f(10))
 
 
-def mul(*values) :
-    a = 1
-    for i in values :
-        a *= i
-    return a
+# def f1(x1) :
+#     return (x1 ** 2) + (2 * x1) + 1
+# print(f1(10))
+# # 5,7,9,10 을 가변 매개변수로 모두 곱한 값을 리턴하는 함수
 
-print(mul(5,7,9,10))
+# def mul(*values) :
+#     i = 1
+#     for j in values :
+#         i *= j
+#     return i
+# print(mul(5, 7, 9, 10))
+
+# def factorial(n):
+#     output = 1
+#     for i in range(1, n + 1):
+#         output *= i
+#     return output
+
+# print(factorial(6))
+
+# def factorial(n):
+#     if n == 0 :
+#        return 1
+
+#     else :
+#         return n * factorial(n - 1)
+
+# print(factorial(5))
+
+
+# def fibonacci(n) :
+# #피보나치 수를 구합니다.
+#     if n == 1 :
+#         return 1
+#     if n == 2 :
+#         return 1
+#     else :
+#         return fibonacci(n - 1) + fibonacci(n - 2)
+# print(fibonacci(5))
+
+# a = {
+#     1 : 1,
+#     2 : 1,
+# }
+
+# def fibonacci(n):
+#     if n in a :
+#         # 메모가 되어 있으면 메모된 값을 리턴
+#         return a[n]
+    
+#     # 메모가 되어 있지 않으면 값을 구함 (메모를 함)
+#     a[n] = fibonacci(n - 1) + fibonacci(n - 2)
+#     return a[n]
+# print(fibonacci(10))
+
+
+# def number_input():
+#     output = float(input("숫자를 입력하세요 > "))
+#     return output
+# def get_circumference(radius):
+#     return 2 * 3.14 * radius
+# def get_circumarea(radius):
+#     return 3.14 * radius * radius
+
+# radius = number_input()
+# print(round(get_circumference(radius),2))
+# print(round(get_circumarea(radius),2))
+
+# # 코드 유지보수를 위해 변수를 활용
+# def p(content):
+#     return f"<p class ='content-line'>{content}</p>"
+# # 아래 한 줄씩 다 바꿔줄 필요없이 위 함수에 변수만 바꿔주면 모두 적용
+# print(p("안녕하세요"))
+# print(p("간단한 HTML 태그를 만드는 중입니다."))
+
+# def flatten(data):
+#     output = []
+#     for i in data:
+#         if type(i) == list:
+#             output += flatten(i)
+#         else:
+#             output += [i]
+#     return output
+
+# example = [[1, 2, 3], [4, [5, 6]], [7, [8, 9]]]
+# print(example)
+# print(flatten(example))
+
+min_to_seat = 2
+max_to_seat = 10
+all_people = 100
+memo = {}
+
+def do(rest_people, seated_people):
+    key = rest_people,seated_people
+    if key in memo :
+        return memo[key]
+    if rest_people < 0 :
+        return 0
+    if rest_people == 0 :
+        return 1
+    
+    count = 0
+    for i in range(seated_people, max_to_seat + 1) :
+        count += do(rest_people - i, i)
+
+    memo[key] = count
+    return count
+print(do(100,2))
+    
