834fe64735e48003ba9da6a5d21aefa7d4941a81
Gyunald <97107396+Gyunald@users.noreply.github.com>
2023-07-14 16:44:23 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -8,128 +8,107 @@ import streamlit as st
 import pandas as pd
 from datetime import time, datetime, timedelta
 
-# prompt = st.chat_input("Say something")
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
 if 'direct' not in st.session_state:
-    st.session_state.direct = []
-if 'indirect' not in st.session_state:
-    st.session_state.indirect = []
+    st.session_state.direct = pd.DataFrame(columns=['시작시간', '종료시간','비고'])
 
-
-
-def direct_schedule(start_time, end_time):
+def direct_schedule(start_time, end_time, s2='', s3=''):
     schedule = st.session_state.direct
-
-    breaks = [start_time,'10:00', '10:10', '11:45', '12:45', '15:00', '15:10', '17:00', '17:20',end_time]
+    breaks = [s2, start_time, '10:00', '10:10', '11:45', '12:45', '15:00', '15:10', '17:00', '17:20', s3, end_time]
     breaks = sorted(list(set(breaks)))
-    try:
-        if start_time < end_time:
-            for i in range(len(breaks) - 1):
-                if start_time <= breaks[i] < end_time:
-                    if breaks[i] == '11:45' or breaks[i] == '17:00':
-                        continue
-                    if start_time < breaks[i+1] :
-                        schedule.append({'시작시간': breaks[i], '종료시간': breaks[i + 1]})
 
-            if len(schedule) == 0 or schedule[0]['종료시간'] <= schedule[0]['시작시간']:
-                schedule = [{'시작시간': start_time, '종료시간': end_time}]
+    if s2 < end_time:
+        for i in range(len(breaks) - 1):
+            if start_time <= breaks[i] < end_time:
+                if breaks[i] == '11:45' or breaks[i] == '17:00':
+                    continue
+                elif start_time < breaks[i + 1]:
+                    schedule = pd.concat([pd.DataFrame([{'시작시간': breaks[i], '종료시간': breaks[i + 1]}]), schedule])
 
-            return schedule
-            
-        else:
-            e.error('시간 확인')  
-        
-    except:
-        return None
+        return schedule
+    else:
+        st.error('시간 확인')
 
-def indirect_schedule(start_time2, end_time2):
-    schedule2 = st.session_state.indirect
-    breaks = [start_time2,'10:00', '10:10', '11:45', '12:45', '15:00', '15:10', '17:00', '17:20',end_time2]
-    breaks = sorted(list(set(breaks)))
-    try:
-        if start_time2 < end_time2:
-            for i in range(len(breaks) - 1):
-                if start_time2 <= breaks[i] < end_time2:
-                    if breaks[i] == '11:45' or breaks[i] == '17:00':
-                        continue
-                    if start_time2 < breaks[i+1] :
-                        schedule2.append({'시작시간': breaks[i], '종료시간': breaks[i + 1]})
+# Streamlit UI
+c1, c2 = st.columns(2)
 
-            if len(schedule2) == 0 or schedule2[0]['종료시간'] <= schedule2[0]['시작시간']:
-                schedule2 = [{'시작시간': start_time2, '종료시간': end_time2}]
+with c1:
+    start_time = st.time_input('시작 시간', time(8, 0), step=600).strftime('%H:%M')
+    end_time = st.time_input('종료 시간', time(20, 20), step=600).strftime('%H:%M')
+    finish = st.checkbox('가공완료')
+    indirect_check = st.checkbox('간접작업')
 
-            return schedule2
-            
-        else:
-            e.error('시간 확인')
-        
-    except:
-        return None
-
-c1,c2,c3 = st.columns(3)
-
-with c1 :
-    name = st.selectbox('이름',['김규덕','김진석'])
-    so = st.text_input('샵오더') 
+with c2:
+    name = '김규덕'
+    so = st.text_input('샵오더')
     work = st.text_input('상세내용')
     id ='kdkim'
     pw = 'xjsld12#'
-    
-with c2 :
-    start_time = st.time_input('시작 시간',time(8,0),step=600).strftime('%H:%M')
-    end_time = st.time_input('종료 시간',time(20,20),step=600).strftime('%H:%M')    
-    finish = st.checkbox('가공완료')
-    indirect_chech = st.checkbox('간접작업')
 
-    # etc = st.text_input('비고')
+if not indirect_check :
+    pass
+    # new_schedule = direct_schedule(start_time, end_time)
 
-c4,c5,c6 = st.columns(3)
+c4, c5 = st.columns(2)
 
-with c4: 
-    e = st.empty()
-    # start_time = e.time_input('시작 시간',key='s1').strftime('%H:%M')
-    # end_time = e.time_input('종료 시간',key='e1').strftime('%H:%M')
-    e.empty()
+with c4:
+    if indirect_check:        
+        e = st.empty()
+        indirect = e.selectbox('간접작업', ['타운홀미팅', '직무교육','회의', '기타'])
+        start_time2 = st.time_input('간접작업 시작', time(8, 0), step=600)
+        end_time2 = st.time_input('간접작업 종료',
+                                (datetime.combine(datetime.min, start_time2) + timedelta(minutes=30)).time(),
+                                step=600).strftime('%H:%M')
+        add_button = st.button('추가', use_container_width=True)
 
-with c5 :
-    if indirect_chech :
-        e2 = st.empty()
-        indirect = e2.selectbox('간접작업',['타운홀미팅','직무교육','기타'])
-        start_time2 = st.time_input('간접작업 시작',time(14,20), step=600).strftime('%H:%M')
-        end_time2 = st.time_input('간접작업 종료',time(15,20), step=600).strftime('%H:%M')
-        run_indirect = st.button('등록',use_container_width=True,key='b1')
         if indirect == '기타':
-            e2.text_input('기타')
+            indirect = e.text_input('기타')
 
-        if run_indirect :          
-            st.session_state.direct.clear()  
-            st.session_state.indirect.clear()
-            timetable = direct_schedule(start_time, end_time)
-            timetable2 = indirect_schedule(start_time2,end_time2)
-            df = pd.DataFrame(timetable+timetable2).drop_duplicates()
-            df = df.sort_values(['시작시간', '종료시간']).reset_index(drop=True)
-            for i in range(1, len(df)):
-                if df.loc[i-1, '종료시간'] > df.loc[i, '시작시간'] and df.loc[i-1,'시작시간'] != df.loc[i,'시작시간']:
-                    df.loc[i-1, '종료시간'] = df.loc[i, '시작시간']
+        if add_button:
+            with c5:
+                if add_button:
+                    new_schedule = direct_schedule(start_time, end_time, start_time2.strftime('%H:%M'), end_time2)
+                    st.session_state.direct = pd.concat([st.session_state.direct, new_schedule]).drop_duplicates().sort_values(['시작시간','종료시간']).reset_index(drop=True)
+                    
+                    for j in st.session_state.direct.loc[st.session_state.direct['시작시간'].duplicated() == True].index:
+                        if j in st.session_state.direct.index:
+                            st.session_state.direct.drop(index=j, inplace=True)
+                    st.dataframe(st.session_state.direct.drop_duplicates().sort_values(['시작시간','종료시간']).reset_index(drop=True))
 
-                elif df.loc[i-1, '종료시간'] > df.loc[i, '시작시간'] and df.loc[i-1,'시작시간'] == df.loc[i,'시작시간']:
-                    df.loc[i, '시작시간'] = df.loc[i-1, '종료시간']
-            with c4:
-                st.dataframe(df)
-    else:        
-        run_direct = st.button('등록',use_container_width=True,key='b2')
 
-        if run_direct :
-            st.session_state.direct.clear()
-            with c4:
-                timetable = direct_schedule(start_time, end_time)
-                df = pd.DataFrame(timetable)
-                st.dataframe(df)
-st.session_state.direct
-st.session_state.indirect
+        else:
+            with c5:
+                st.dataframe(st.session_state.direct)
 
+    else:
+        with c5:
+            if end_time:
+                new_schedule = direct_schedule(start_time, end_time).drop_duplicates().sort_values(['시작시간','종료시간']).reset_index(drop=True)
+                st.dataframe(new_schedule)
+    run_indirect = st.button('등록', use_container_width=True)
+
+    if run_indirect:
+        st.session_state.clear()
+        # st.experimental_rerun()
+
+# df2 = st.session_state.indirect
+# for i in range(1, len(df2)):
+#     # if df2.loc[i - 1, '종료시간'] > df2.loc[i, '시작시간'] and df2.loc[i - 1, '시작시간'] != df2.loc[i, '시작시간']:
+#     #     df2.loc[i - 1, '종료시간'] = df2.loc[i, '시작시간']
+        
+#     # elif df2.loc[i - 1, '종료시간'] < df2.loc[i, '시작시간'] and df2.loc[i - 1, '시작시간'] == df2.loc[i, '시작시간']:
+#     #     df2.loc[i, '시작시간'] = df2.loc[i - 1, '종료시간']
+        
+#     # if df2.loc[i-1, '종료시간'] == df2.loc[i-1, '시작시간']:
+#     #     df2 = df2.drop(i-1)
+       
+#     if df2.loc[i, '시작시간'] > df2.loc[i-1,'종료시간']:
+#         df2.loc[i, '시작시간'] = df2.loc[i-1, '종료시간']
+#     elif  df2.loc[i, '시작시간'] == df2.loc[i, '종료시간']:
+#         df2 = df2.drop(i-1)
+# st.dataframe(df2.drop_duplicates().reset_index(drop=True))
+
+
+# # st.session_state.direct
 # if run :    
 #     chrome_optiins = Options() # 브라우저 꺼짐 방지
 #     chrome_optiins.add_experimental_option('detach', True)
